c2fe74698a4d88251fe050dca65a770df414a657
Adam <adam@playnimbus.com>
2014-08-02 01:45:16 -0400

    Getting there on the veering movement.

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -3,31 +3,34 @@ using System.Collections;
 
 public class ShipMovement : MonoBehaviour
 {
-    // Speed in m/s
-    [Range(0f, 25f)] public float speed;
+    public GameObject pivot;
+    public GameObject ship;
 
-    // Amount at which to turn in degrees
-    [Range(0f, 90f)] public float turnAngle;
+    public float speed;
 
-    [Range(0f, 20f)]
-    public float turnSpeed;
-
-    Plane movementPlane;
+    [Range(-90f, 90f)]
+    public float turnAngle;
 
     // The primary direction the ship will be moving in, in degrees
     private float primaryDirection = 90f;
 
-    private float herpAmount = 0f;
-    
+    Plane movementPlane;
+
+    [Range(1f, 10f)]
+    public float planeDistance = 3f;
+
+    private float serpAmount = 0f;
+
+    [Range(1f, 10f)]
+    public float turnSpeed = 3f;
+
     void Start()
     {
-        UpdatePlane();
+
     }
     
 	void FixedUpdate () 
 	{
-        movementPlane.distance = transform.position.magnitude + speed;
-
 	    if(Input.GetMouseButton(0))
         {
             Vector3 viewportPoint = Camera.main.ScreenToViewportPoint(Input.mousePosition);
@@ -35,49 +38,50 @@ public class ShipMovement : MonoBehaviour
         }
         else
         {
-            Steer();
+            Steer(new Vector3(0.5f, 0.5f, 0f));
         }
 
         Move();
 	}
 
-    void UpdatePlane()
-    {
-        movementPlane.normal = new Vector2(Mathf.Cos(Mathf.Deg2Rad * primaryDirection), Mathf.Sin(Mathf.Deg2Rad * primaryDirection));
-        movementPlane.distance = movementPlane.distance + speed;
-    }
-
     void Move()
     {
-        float angle = primaryDirection + Hermite(0f, turnAngle * Mathf.Sign(herpAmount), Mathf.Abs(herpAmount));
-        Vector2 direction = new Vector2(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle));
-        Vector2 velocity = direction * speed * Time.fixedDeltaTime;
+        Vector2 normal = new Vector2(Mathf.Cos(Mathf.Deg2Rad * primaryDirection), Mathf.Sin(Mathf.Deg2Rad * primaryDirection));
 
-        rigidbody2D.MovePosition((Vector2)transform.position + velocity);
-        transform.up = direction;
-    }
+        pivot.transform.position = (Vector2)pivot.transform.position + normal * speed * Time.fixedDeltaTime;
+        movementPlane.SetNormalAndPosition(normal, (Vector2)pivot.transform.position + normal * planeDistance);
 
-    void Steer()
-    {
-        Steer(new Vector3(0.5f, 0.5f, 0f));
+        float movementAngle = primaryDirection + Sinerp(0f, Mathf.Sign(serpAmount) * turnAngle, Mathf.Abs(serpAmount));
+        Vector2 movementDirection = new Vector2(Mathf.Cos(Mathf.Deg2Rad * movementAngle), Mathf.Sin(Mathf.Deg2Rad * movementAngle));
+        Ray ray = new Ray(pivot.transform.position, movementDirection);
+
+        pivot.transform.position = pivot.transform.position + Vector3.right * Mathf.Cos(Mathf.Deg2Rad * movementAngle);
+
+        float distance;
+        if(movementPlane.Raycast(ray, out distance))
+        {
+            Vector3 position = ray.GetPoint(distance);
+            ship.rigidbody2D.MovePosition(position);
+            ship.transform.up = movementDirection;
+        }
     }
 
     void Steer(Vector3 viewportPoint)
     {
-        if(viewportPoint.x < 0.2f)
+        if(viewportPoint.x < 0.4f)
         {
-            herpAmount += Time.fixedDeltaTime * turnSpeed;
+            serpAmount += Time.fixedDeltaTime * turnSpeed;
         }
-        else if(viewportPoint.x > 0.8f)
+        else if(viewportPoint.x > 0.6f)
         {
-            herpAmount -= Time.fixedDeltaTime * turnSpeed;
+            serpAmount -= Time.fixedDeltaTime * turnSpeed;
         }
         else
         {
-            herpAmount = Mathf.MoveTowards(herpAmount, 0f, Time.fixedDeltaTime * turnSpeed);
+            serpAmount = Mathf.MoveTowards(serpAmount, 0f, Time.fixedDeltaTime * turnSpeed);
         }
 
-        herpAmount = Mathf.Clamp(herpAmount, -1f, 1f);
+        serpAmount = Mathf.Clamp(serpAmount, -1f, 1f);
     }
 
     // *****************************************************
