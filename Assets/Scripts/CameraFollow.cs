23c6e9353a87ddb59a1ec40d5e406317babe302a
Adam <adam@playnimbus.com>
2014-08-02 00:32:29 -0400

    Snapshot partway through prototyping.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject objectToFollow;
+    Vector3 offset;
 
-	void Update () 
+    void Start()
+    {
+        offset = transform.position - objectToFollow.transform.position;
+    }
+
+	void FixedUpdate () 
 	{
         UpdatePosition();
 	}
 
     void UpdatePosition()
     {
-        Vector3 objectPosition = objectToFollow.transform.position;
-        objectPosition.z = transform.position.z;
+        Vector3 objectPosition = objectToFollow.transform.position + offset;
         Vector3 lerpedPosition = Vector3.Lerp(transform.position, objectPosition, 0.25f);
         transform.position = lerpedPosition;
     }
--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -3,40 +3,94 @@ using System.Collections;
 
 public class ShipMovement : MonoBehaviour
 {
-    public float turningPower;
-    public float turningThrust;
-    public float thrustingPower;
+    // Speed in m/s
+    [Range(0f, 25f)] public float speed;
 
+    // Amount at which to turn in degrees
+    [Range(0f, 90f)] public float turnAngle;
+
+    [Range(0f, 20f)]
+    public float turnSpeed;
+
+    Plane movementPlane;
+
+    // The primary direction the ship will be moving in, in degrees
+    private float primaryDirection = 90f;
+
+    private float herpAmount = 0f;
+    
+    void Start()
+    {
+        UpdatePlane();
+    }
+    
 	void FixedUpdate () 
 	{
-        ApplyForwardForce();
+        movementPlane.distance = transform.position.magnitude + speed;
 
 	    if(Input.GetMouseButton(0))
         {
-            ApplySteeringForce(Camera.main.ScreenToViewportPoint(Input.mousePosition));        
+            Vector3 viewportPoint = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            Steer(viewportPoint);        
         }
+        else
+        {
+            Steer();
+        }
+
+        Move();
 	}
 
-    void ApplyForwardForce()
+    void UpdatePlane()
     {
-        rigidbody2D.AddForce(Vector3.up, ForceMode.Force);
+        movementPlane.normal = new Vector2(Mathf.Cos(Mathf.Deg2Rad * primaryDirection), Mathf.Sin(Mathf.Deg2Rad * primaryDirection));
+        movementPlane.distance = movementPlane.distance + speed;
     }
 
-    void ApplySteeringForce(Vector3 normalizedScreen)
+    void Move()
     {
-        if(normalizedScreen.y < 0.2f)
+        float angle = primaryDirection + Hermite(0f, turnAngle * Mathf.Sign(herpAmount), Mathf.Abs(herpAmount));
+        Vector2 direction = new Vector2(Mathf.Cos(Mathf.Deg2Rad * angle), Mathf.Sin(Mathf.Deg2Rad * angle));
+        Vector2 velocity = direction * speed * Time.fixedDeltaTime;
+
+        rigidbody2D.MovePosition((Vector2)transform.position + velocity);
+        transform.up = direction;
+    }
+
+    void Steer()
+    {
+        Steer(new Vector3(0.5f, 0.5f, 0f));
+    }
+
+    void Steer(Vector3 viewportPoint)
+    {
+        if(viewportPoint.x < 0.2f)
         {
-            rigidbody2D.AddForce(Vector2.up * thrustingPower, ForceMode.Force, Space.Self);
+            herpAmount += Time.fixedDeltaTime * turnSpeed;
         }
-        else if(normalizedScreen.x < 0.2f)
+        else if(viewportPoint.x > 0.8f)
         {
-            rigidbody2D.AddTorque(turningPower);
-            rigidbody2D.AddForce(Vector2.up * turningThrust, ForceMode.Force, Space.Self);
+            herpAmount -= Time.fixedDeltaTime * turnSpeed;
         }
-        else if(normalizedScreen.x > 0.8f)
+        else
         {
-            rigidbody2D.AddTorque(-turningPower);
-            rigidbody2D.AddForce(Vector2.up * turningThrust, ForceMode.Force, Space.Self);
+            herpAmount = Mathf.MoveTowards(herpAmount, 0f, Time.fixedDeltaTime * turnSpeed);
         }
+
+        herpAmount = Mathf.Clamp(herpAmount, -1f, 1f);
+    }
+
+    // *****************************************************
+    // From: http://wiki.unity3d.com/index.php?title=Mathfx
+    // *****************************************************
+
+    public float Hermite(float start, float end, float value)
+    {
+        return Mathf.Lerp(start, end, value * value * (3.0f - 2.0f * value));
+    }
+
+    public static float Sinerp(float start, float end, float value)
+    {
+        return Mathf.Lerp(start, end, Mathf.Sin(value * Mathf.PI * 0.5f));
     }
 }
